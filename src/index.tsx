92cd2b26b02f855f59e27afb5ffb7a44ba70c9b5
aaron abiyoga <aaron.sulistio@shopee.com>
2022-11-19 06:33:17 +0700

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import Store from './Redux';
@@ -19,6 +21,8 @@ root.render(
     </Provider>
   </BrowserRouter>,
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
